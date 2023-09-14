ad5c7675518701f54479fba3c30513595dd96b54
anya <riidloa@gmail.com>
2023-09-14 16:24:40 +0700

    tugas 4

---
+++ components/FooterWeb.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <span class="navbar-text"> &copy; 2022 Taskist. All right reserved </span>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "Hello World!",
+    };
+  },
+};
+</script>
---
+++ layouts/custom.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+    <HeaderWeb />
+    <Nuxt />
+    <FooterWeb />
+  </div>
+</template>
+<script>
+export default {};
+</script>
